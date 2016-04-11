5acd917c0516fcfacce87b418d7ac4e41266cf1e
jingzing <louiswang0898@gmail.com>
2016-04-11 18:22:23 +0800

    spring redis

--- Spring-Boot-Redis/src/main/java/com/jingzing/Application.java
+++ Spring-Boot-Redis/src/main/java/com/jingzing/Application.java
@@ -1,16 +1,16 @@
 package com.jingzing;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Spring-Boot
  * Created by Louis Wang on 2016/4/8.
  */
 
-@RestController
-@EnableAutoConfiguration
+/*@RestController
+@EnableAutoConfiguration*/
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws Exception {
